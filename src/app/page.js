c6fd318863ba5338e98d3273cddf708f99495c67
rohitrrr345 <rrajput2060@gmail.com>
2024-09-04 23:59:31 +0530

    this is new Project

---
+++ src/app/page.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <main className='p-4 max-w-2xl mx-auto'>
+  <header className='flex justify-between mb-4'>
+ 
+
+  <a href="" className='flex gap-1'> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
+</svg>
+<span>Epic creations</span>
+
+</a>
+
+   <nav className='flex gap-6 text-white/80'>
+   <a href="">Home</a>
+    <a href="">Pricing</a>
+    <a href="">Contact</a>
+   </nav>
+
+  </header>
+  <div className='text-center mt-24 mb-8'>
+    <h1 className='text-3xl'>Add epic captions to your videos</h1>
+    <h2 className='text-white/75'>Just upload the Video</h2>
+
+  </div>
+  <div className='text-center'>
+    <button className='bg-green-600 py-2 px-6 rounded-full inline-flex gap-2 border-2 border-purple-700/50'>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z" />
+</svg>
+
+   <span>Choose file</span>
+    </button>
+  </div>
+    </main>
+  )
+}
+
+export default page
